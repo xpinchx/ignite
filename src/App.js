fc9b557657f8896cad83aed8ec11a29f7ce3c5d9
xpinchx <dougwelch@gmail.com>
2021-09-13 15:18:49 -0500

    Added nav and search bar with styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+// React
 import React from "react";
+
+// Router
+import { Route } from "react-router-dom";
 
 // Components and Pages
 import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
-import { Route } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
